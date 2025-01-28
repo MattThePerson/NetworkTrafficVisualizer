a780499203add97652d8bb1fdfc52cf2dac073ee
Matt Stirling <stirlingmattmarkus@gmail.com>
2025-01-28 22:49:55 +0200

    fixed packet to json conversion, added text_mode

--- lib/sniffer.py
+++ lib/sniffer.py
@@ -2,36 +2,38 @@ from typing import Any
 import scapy.all as scapy
 import json
 
-def hex_string_to_binary_string_SUPERFUCKINGOPTIMIZED_ONELINERRRRR(hex_str: str):
+def hex_string_to_binary_string(hex_str: str):
     return ''.join([ '{:0>4b}'.format( int(hex_dig, 16)) for hex_dig in hex_str ])
 
 
-def hex_string_to_binary_string(hex_str: str):
-    def hex_digit_to_binary(hex_str: str):
-        integer = int(hex_str, 16)
-        return '{:0>4b}'.format(integer)
-    binary_str = ''
-    for hex_digit in hex_str:
-        binary_str += hex_digit_to_binary(hex_digit)
-    return binary_str
+def packet_to_json(packet: scapy.Packet) -> dict[str, Any] | None:
+    obj = None
+    for layer in reversed(packet.layers()):
+        curr_obj = packet[layer].fields
+        if obj:
+            curr_obj['payload'] = obj
+        obj = curr_obj
+    obj = json.loads(json.dumps(obj, default=str))
+    return obj
 
 
-def packet_to_json(packet: scapy.Packet) -> dict[str, Any]:
+def scapy_packet_to_json(packet: scapy.Packet) -> dict[str, Any]:
     obj: dict[str, Any] = {}
     obj['summary'] = packet.summary()
     obj['sniffed_on'] = str(packet.sniffed_on)
     obj['time'] = packet.time
     obj['len'] = len(bytes(packet))
     obj['hex'] = bytes(packet).hex()
-    obj['binary'] = hex_string_to_binary_string_SUPERFUCKINGOPTIMIZED_ONELINERRRRR(obj['hex'])
-    obj['json'] = json.loads(packet.json())
+    obj['binary'] = hex_string_to_binary_string(obj['hex'])
+    obj['json'] = packet_to_json(packet)
     return obj
 
 
 def packet_sniffer(queue: Any):
     def handle_packet(packet: scapy.Packet):
-        # print(packet.summary())
-        queue.put(packet_to_json(packet))
+        packet_obj = scapy_packet_to_json(packet)
+        if packet_obj:
+            queue.put(packet_obj)
     
     scapy.sniff(
         prn=handle_packet,
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from typing import Any
 import argparse
 from multiprocessing import Process, Queue
 import signal
+import time
 from lib import AppState
 from lib import CursesRenderer
 from lib import App
 from lib import packet_sniffer
-
 
 
 # MAIN
@@ -16,24 +16,39 @@ def main(args: argparse.Namespace):
     state = AppState()
     renderer = CursesRenderer()
     
-    queue: Queue[Any] = Queue() # messages from sniffer process
+    sniffer_messages: Queue[Any] = Queue() # messages from sniffer process
     sniffer_process = Process(
         target=packet_sniffer,
-        args=(queue,)
+        args=(sniffer_messages,)
     )
 
-    signal.signal(signal.SIGINT, lambda s, f: app.stop()) # handle ctrl+c
-    
     # start shit
     sniffer_process.start()
+    
+    if args.text_mode:
+        text_mode(sniffer_messages)
+    else:
+        gui_mode(app, state, renderer, sniffer_messages)
+    
+    sniffer_process.terminate()
+    sniffer_process.join()
+    print('App closed. Goodbye!')
+
+
+
+# GUI MODE
+def gui_mode(app: App, state: AppState, renderer: CursesRenderer, sniffer_messages: Any):
+    """ Sniffer messages is of type multiprocess.Queue[dict[str, Any]] """
+    signal.signal(signal.SIGINT, lambda s, f: app.stop()) # handle ctrl+c
+    
     renderer.init()
     app.start()
     
     # update loop
     while app.running():
         
-        while not queue.empty():
-            state.add_packet(queue.get_nowait()) # why nowait?
+        while not sniffer_messages.empty():
+            state.add_packet(sniffer_messages.get_nowait()) # why nowait?
         
         if not app.paused:
             state.update() # pass in app.time() ?
@@ -55,39 +70,54 @@ def main(args: argparse.Namespace):
 
     # end shit
     renderer.end()
-    sniffer_process.terminate()
-    sniffer_process.join()
-    print('App closed. Goodbye!')
+    return
+
+
+
+# TEXT MODE
+def text_mode(sniffer_messages: Any):
+    """ Sniffer messages is of type multiprocess.Queue[dict[str, Any]] """
+    print('Listening for packets ...')
+    try:
+        while True:
+            while not sniffer_messages.empty():
+                packet_obj = sniffer_messages.get()
+                print(packet_obj['summary'])
+            time.sleep(1/60)
+    except KeyboardInterrupt:
+        print('\n... interrupted with CTRL-C')
+        return
 
 
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser("Python script for visualizing the flow of traffic over your network!")
+    parser = argparse.ArgumentParser("Python script for visualizing the flow of traffic over your network right in the terminal!")
 
     # filter
     parser.add_argument("--iface", help="")
     parser.add_argument("--filter", help="")
     parser.add_argument("--protocol", help="")
     parser.add_argument("--port", help="")
-    parser.add_argument("--direction", help="incoming or outgoing packets") # inputs?
+    parser.add_argument("--incoming-only", action="store_true", help="incoming or outgoing packets")
+    parser.add_argument("--outgoing-only", action="store_true", help="incoming or outgoing packets")
     parser.add_argument("--packets-larger-than", help="", type=int)
     parser.add_argument("--packets-smaller-than", help="", type=int)
+    parser.add_argument("--count", help="", type=int)
     
     # display
-    parser.add_argument("--vertical", action='store_true', help="") # (maybe not?)
-    parser.add_argument("--text-mode", action='store_true', help="list caught packets in a wireshark-esque way")
-    parser.add_argument("--fade-time", action='store_true', help="")
-    parser.add_argument("--byte-mode", action='store_true', help="represent data stream as bytes instead of bits")
+    parser.add_argument("--text-mode", action="store_true", help="list caught packets in a wireshark-esque way")
+    parser.add_argument("--fade-time", action="store_true", help="")
+    parser.add_argument("--byte-mode", action="store_true", help="represent data stream as bytes instead of bits")
     parser.add_argument("--max-exchange-stacking'", help="max thickness of a 'relationship", type=int)
-    parser.add_argument("--non-stacking-mode", action='store_true', help="don't group (stack) exchanges in same relationship")
-    parser.add_argument("--no-ip-resolve", action='store_true', help="Dont show IPs resolved names")
+    parser.add_argument("--non-stacking-mode", action="store_true", help="don't group (stack) exchanges in same relationship")
+    parser.add_argument("--no-ip-resolve", action="store_true", help="Dont show IPs resolved names")
     
     # logging
     parser.add_argument("--log-file", help="")
     parser.add_argument("--pcap", help="Export to a .pcap file for later analysis in Wireshark")
-    parser.add_argument("--list-ifs", action='store_true', help="")
-    parser.add_argument("--simulate-log", action='store_true', help="reads packets from log file and simulates their arrival")
+    parser.add_argument("--list-ifs", action="store_true", help="")
+    parser.add_argument("--simulate-log", action="store_true", help="reads packets from log file and simulates their arrival")
 
     args = parser.parse_args()
     # print('calling main() ...')
